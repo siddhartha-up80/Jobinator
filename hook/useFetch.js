890bfda1be67d571cfcdc8db032c9c4044590a5c
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-05-14 17:15:49 +0530

    Jobinator UI Updated

---
+++ hook/useFetch.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import { RAPID_API_KEY } from "@env"
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    // url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    url: `https://jsearch.p.rapidapi.com/search`,
+    headers: {
+      //   "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Key": "9a26b8c98emsh11e9fcaf5622951p17f35bjsn146ed62cba53",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: {
+    //   query: { ...query },
+      query: "React Developer, Bhopal",
+      page: "1",
+      num_pages: "1",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
